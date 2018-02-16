361199a262be14849152ab24d66cf482ca29f032
William Antony <william@pathadan.com>
2018-02-16 17:03:22 -0600

    Resolved redux import bug

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
 import ReduxThunk from 'redux-thunk';
-import reducers from './redux/reducers/users.reducer';
+import reducers from './redux/reducers';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
